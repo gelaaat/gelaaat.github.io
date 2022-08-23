49155c10dd3ee0573de128272bf8e6d4370bcb12
Arnau <arnaugelabertojuarez@gmail.com>
2022-08-23 15:26:31 +0200

    final move added, trying to get bigger

--- index.js
+++ index.js
@@ -3,48 +3,49 @@ var ctx = canvas.getContext("2d");
 
 
 class Snake{
-    constructor(){
-        this.x = 0;
-        this.y = 0;
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
         this.tail = [{x:this.x, y:this.y}];
         this.direction = [{x:1, y:0}];
-        
+        console.log(this.x);
+        console.log(this.tail[0]);
     }
 
     move(){
-        let newReaction;
+        let newReaction = {};
 
-        if(this.direction.x === 0 && this.direction.y === 1){
+        if(this.direction[0].x === 0 && this.direction[0].y === 1){
             newReaction = {
-                x: snake.tail[snake.tail.length - 1].x,
-                y: snake.tail[snake.tail.length - 1].y + 20
-            }
-        }
-        else if(this.direction.x === 0 && this.direction.y === -1){
-            newReaction = {
-                x: snake.tail[snake.tail.length - 1].x,
-                y: snake.tail[snake.tail.length - 1].y - 20
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y + 20
             } 
         }
-        else if(this.direction.x === -1 && this.direction.y === 0){
+        else if(this.direction[0].x === 0 && this.direction[0].y === -1){
             newReaction = {
-                x: snake.tail[snake.tail.length - 1].x - 20,
-                y: snake.tail[snake.tail.length - 1].y
+                x: this.tail[this.tail.length - 1].x,
+                y: this.tail[this.tail.length - 1].y - 20
+            }  
+        }
+        else if(this.direction[0].x === -1 && this.direction[0].y === 0){
+            newReaction = {
+                x: this.tail[this.tail.length - 1].x - 20,
+                y: this.tail[this.tail.length - 1].y
+            }  
+        }
+        else if(this.direction[0].x === 1 && this.direction[0].y === 0){
+            newReaction = {
+                x: this.tail[this.tail.length - 1].x + 20,
+                y: this.tail[this.tail.length - 1].y
             } 
         }
-        else if(this.direction.x === 1 && this.direction.y === 0){
-            newReaction = {
-                x: snake.tail[snake.tail.length - 1].x + 20,
-                y: snake.tail[snake.tail.length - 1].y
-            }
-        }
-
+        this.tail.shift();
         this.tail.push(newReaction);
-
+        
     }
 }
 
-const snake = new Snake();
+const snake = new Snake(10, 10);
 
 
 const createSquare = (x, y, width, height, color) => {
@@ -55,31 +56,31 @@ const createSquare = (x, y, width, height, color) => {
 const update = () => {
     createSquare(0, 0, canvas.width, canvas.height, "black");
     for (let i = 0; i < snake.tail.length; i++) {
+        
         createSquare(snake.tail[i].x, snake.tail[i].y, 15, 15, "#fff");
     }
     
 }
 
 document.addEventListener('keydown', (key)=>{
-    console.log(key.code);
     if(key.code === "ArrowDown"){
-        snake.direction.x = 0;
-        snake.direction.y = 1;
+        snake.direction[0].x = 0;
+        snake.direction[0].y = 1;
         
         
     }else if(key.code === "ArrowUp"){
-        snake.direction.x = 0;
-        snake.direction.y = -1;
+        snake.direction[0].x = 0;
+        snake.direction[0].y = -1;
         
         
     }else if(key.code === "ArrowLeft"){
-        snake.direction.x = -1;
-        snake.direction.y = 0;
+        snake.direction[0].x = -1;
+        snake.direction[0].y = 0;
         
         
     }else if(key.code === "ArrowRight"){
-        snake.direction.x = 1;
-        snake.direction.y = 0;
+        snake.direction[0].x = 1;
+        snake.direction[0].y = 0;
         
     }
 })
@@ -90,7 +91,7 @@ const gameLoop = () => {
         snake.move();
         update();
         
-    }, 1000);
+    }, 500);
 }
 
 gameLoop();
